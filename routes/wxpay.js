507c7f1552664f8ac11eebfaaf37881ed5d25e4a
sudasf <1518223884@qq.com>
2017-02-09 13:25:14 +0800

    change

---
+++ routes/wxpay.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	var {params,query,body}=req
+	console.log(params,query,body);
+  res.render('index', { title: 'xixi' });
+});
+
+module.exports = router;
