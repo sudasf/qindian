264531f2b9a9fe4d5050e407fd3863488e43630f
sudasf <1518223884@qq.com>
2017-02-09 14:15:26 +0800

    change

--- routes/wxpay.js
+++ routes/wxpay.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var parseString = require('xml2js').parseString;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,10 +12,33 @@ router.get('/', function(req, res, next) {
 router.post('/:id', function(req, res, next) {
 	var {params,query,body}=req
 	//console.log(req);
-	console.log(params,query,body);
-  	res.json({
-  		code:1
-  	})
+	var order_id=req.params.id
+	var buf = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) { 
+		buf += chunk;
+	});
+	// 内容接收完毕
+    req.on('end', function() {
+		xml2js.parseString(buf, function(err, json) {
+			if (err) {
+                err.status = 400;
+            } else {
+                req.body = json;
+                console.log(json);
+            	var output='<xml>'+
+				 '<return_code><![CDATA[SUCCESS]]></return_code>'+
+				 '<return_msg><![CDATA[OK]]></return_msg>'+
+				'</xml>'
+				res.type('xml'); 
+			  	res.send(output)
+            }
+        });
+		
+		
+    });
+
+
 });
 
 module.exports = router;
